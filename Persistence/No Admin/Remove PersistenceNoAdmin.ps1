9cf2dda62980b3fd4ca3ca0cce738f4b98d97d47
xp4xbox <nicman1080@gmail.com>
2017-09-20 21:53:30 -0400

    Update Remove PersistenceNoAdmin.ps1

--- Persistence/No Admin/Remove PersistenceNoAdmin.ps1
+++ Persistence/No Admin/Remove PersistenceNoAdmin.ps1
@@ -1,4 +1,7 @@
 $strFileName = (Get-Item -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Run).GetValue("winupdate")
+
+# kill and delete program
+Get-Process | Where-Object {$_.Path -like $strFileName} | Stop-Process -Force
 Remove-Item $strFileName -Force
 
 Remove-ItemProperty -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Run -name winupdate
