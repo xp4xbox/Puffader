c827b5f25ccb15e0941bf82d5df3b38d1cff598e
xp4xbox <nicman1080@gmail.com>
2017-09-23 10:26:10 -0400

    Update remover for #5a465d5

--- Persistence/Remove Persistence.ps1
+++ Persistence/Remove Persistence.ps1
@@ -3,10 +3,16 @@ If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     Start-Process powershell.exe -Verb RunAs -ArgumentList ('-WindowStyle Hidden -noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
     exit
 }
+
 $strFileName = (Get-Item -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Run).GetValue("winupdate")
-# kill and delete program
+Write-Output $strFileName
+# kill the program
 Get-Process | Where-Object {$_.Path -like $strFileName} | Stop-Process -Force
-Remove-Item $strFileName -Force
+
+$strFileDir = Split-Path $strFileName
+
+# delete folder
+Remove-Item $strFileDir -Recurse -Force
 
 # stop program from starting at startup
 Remove-ItemProperty -path HKLM:\Software\Microsoft\Windows\CurrentVersion\Run -name winupdate
