d3c68d63f15012fa0c9f90993b8f42c247b85fa5
xp4xbox <nicman1080@gmail.com>
2017-09-22 21:25:43 -0400

    Remove Unsupported Directory

--- Persistence/No Admin/Remove PersistenceNoAdmin.ps1
+++
@@ -1,7 +0,0 @@
-$strFileName = (Get-Item -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Run).GetValue("winupdate")
-
-# kill and delete program
-Get-Process | Where-Object {$_.Path -like $strFileName} | Stop-Process -Force
-Remove-Item $strFileName -Force
-
-Remove-ItemProperty -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Run -name winupdate
