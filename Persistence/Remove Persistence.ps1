0629b9d268e0caa0380a87cc3066261a8020ee6a
xp4xbox <nicman1080@gmail.com>
2017-10-11 17:46:50 -0400

    Update Remover for 4b25722

--- Persistence/Remove Persistence.ps1
+++ Persistence/Remove Persistence.ps1
@@ -1,11 +1,20 @@
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
 {
+    $strFileName = (Get-Item -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Run).GetValue("winupdate")
+    Get-Process | Where-Object {$_.Path -like $strFileName} | Stop-Process -Force
+
+    $strFileDir = Split-Path $strFileName
+    Remove-Item $strFileDir -Recurse -Force
+
+    Remove-ItemProperty -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Run -name winupdate
+     
+
     Start-Process powershell.exe -Verb RunAs -ArgumentList ('-WindowStyle Hidden -noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
     exit
 }
 
 $strFileName = (Get-Item -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Run).GetValue("winupdate")
-Write-Output $strFileName
+
 # kill the program
 Get-Process | Where-Object {$_.Path -like $strFileName} | Stop-Process -Force
 
