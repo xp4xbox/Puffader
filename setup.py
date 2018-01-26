a925f346d1e5bbf8893d330901f87eebba4e77a5
xp4xbox <nicman1080@gmail.com>
2018-01-26 13:19:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ import py2exe
 setup(script_args = ['py2exe'],
       windows=[{'script':'Puffader.py'}],
       options = {'py2exe': {'excludes': ['Tkconstants', 'Tkinter'],
-							'includes':['email.mime.multipart', 'email.mime.image']
+							'includes':['email.mime.multipart', 'email.mime.image'],
+							'bundle_files':1
                             },
                  },
+		zipfile = None,
 		)
